e0a4f991dd9b487db673e6f9dc5b83469a46d262
Corneel Moons <corneel.moons2@gmail.com>
2025-09-09 14:52:02 +0200

    gitignore gd

--- .history/main_20250908123823.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from bsebrushup2025gitresp! test  dit moet alleen in branches staan")
-
-
-if __name__ == "__main__":
-    main()
--- .history/main_20250908124108.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from bsebrushup2025gitresp! test  dit moet overal komen")
-
-
-if __name__ == "__main__":
-    main()
